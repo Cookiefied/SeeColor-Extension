162edd581809c1caecaefd2677bf79406ea4cb77
Bailey Shelton <104048251+bcshelto565@users.noreply.github.com>
2023-03-30 23:08:03 -0500

    Create ColorScript.js
    Testing script for sidebar menu on click in context menu

---
+++ ColorScript.js
@@ -0,0 +1,22 @@
+function onCreated() {
+  if (browser.runtime.lastError) {
+    console.log(`Error: ${browser.runtime.lastError}`);
+  } else {
+    console.log("Item created successfully");
+  }
+}
+
+browser.menus.create({
+  id: "open-sidebar",
+  title: browser.i18n.getMessage("menuItemOpenSidebar"),
+  contexts: ["all"],
+  command: "_execute_sidebar_action"
+}, onCreated);
+
+browser.menus.onClicked.addListener((info, tab) => {
+  switch (info.menuItemId) {
+      case "open-sidebar":
+        console.log("Opening my sidebar");
+        break;
+  }
+});
