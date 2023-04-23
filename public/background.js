2dbf2a6bf2b9b0b8144200d107b8370409eb228a
Bailey Shelton <104048251+bcshelto565@users.noreply.github.com>
2023-04-22 23:11:39 -0500

    Update background.js
    rewriting overwrite on SeeColor code.

--- public/background.js
+++ public/background.js
@@ -1,19 +1,9 @@
-chrome.contextMenus.create({
-	id: "seeColor",
+console.log("SeeColor extension loaded")
+
+browser.menus.create({
+	id: "SeeColor",
 	title: "Evaluate via SeeColor",
 	contexts: ["image"]
   });
   
-  chrome.contextMenus.onClicked.addListener(function(info, tab) {
-	if (info.menuItemId === "seeColor") {
-		chrome.tabs.sendMessage(tab.id, {action: "seecolor"}, function(response) {
-			chrome.windows.create({
-				tabId: tab.id,
-				type: "popup",
-				height: 600,
-				width: 350,
-				url: "popup.html?imageUrl=" + encodeURIComponent(info.srcUrl)
-			});
-		});
-	}
-});
+  
