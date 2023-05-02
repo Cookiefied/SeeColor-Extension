6e8a41b0b7d10ec7753c76ccbe9dcc68f9de6780
Bailey Shelton <104048251+bcshelto565@users.noreply.github.com>
2023-05-01 23:44:22 -0500

    Add files via upload

--- ColorScript.js
+++ ColorScript.js
@@ -20,3 +20,46 @@ browser.menus.onClicked.addListener((info, tab) => {
         break;
   }
 });
+
+document.getElementById('upload-btn').addEventListener('click', function() {
+  // Open the file picker dialog
+  let input = document.createElement('input');
+  input.type = 'file';
+  input.accept = 'image/*';
+  input.onchange = function() {
+    let file = input.files[0];
+    if (file) {
+      // Process the uploaded image file and open it in a new tab
+      handleImageUpload(file);
+    }
+  };
+  input.click();
+});
+
+
+function handleImageUpload(file) {
+  // Read the file as a data URL
+  let reader = new FileReader();
+  reader.onload = function() {
+    let imageData = reader.result;
+    // Open the uploaded image in a new tab
+    browser.tabs.create({
+      url: imageData,
+      active: true
+    });
+  };
+  reader.readAsDataURL(file);
+}
+
+const fileInput = document.getElementById("myFileInput");
+
+fileInput.addEventListener("change", (event) => {
+  const selectedFile = event.target.files[0];
+  console.log(`Selected file: ${selectedFile.name}`);
+  browser.tabs.create({
+    url: event.target.files[0],
+    active: true
+  });
+  // Do something with the selected file
+  reader.readAsDataURL(file);
+});
